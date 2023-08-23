fac2d8cb998f420919e6c15e8b589c23cf2a3c14
John Kennedy <johnkssilva@gmail.com>
2023-08-23 12:34:20 -0300

    Fix: changing connection from mongodb lib to mongoose lib

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,25 +1,21 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@product-calculator.tphyp0g.mongodb.net/?retryWrites=true&w=majority`;
+const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
-    const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-    });
+    const user = encodeURIComponent(process.env.MONGODB_USER);
+    const pass = encodeURIComponent(process.env.MONGODB_PASS);
 
-    async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
-    }
-    }
-    run().catch(console.dir);
+    const uri = `mongodb+srv://${user}:${pass}@product-calculator.tphyp0g.mongodb.net/?retryWrites=true&w=majority`;
+
+    await mongoose.connect(
+        uri,
+        (error)=>{
+            if(error){
+                return console.log("An error occurred while connecting to the DB: ", error);
+            }
+
+            return console.log("Database connection successful!");
+        }
+    );
 }
 
 module.exports = connectToDatabase;

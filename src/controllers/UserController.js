5181fcb0c81004d121eb5d8d00de98fe7d8100b7
John Kennedy <johnkssilva@gmail.com>
2023-08-27 20:32:11 -0300

    Feat: add crypto lib, signIn, encryptPassword and decryptPassword controllers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,13 @@
 const UserModel = require("../models/user/UserModel");
+const crypto = require("crypto");
 
 const UserController = {
     Create: async (req, res) => {
         const { username, email, password } = req.body;
 
         try {
-            const user_db = await UserModel.create({ username, email, password });
+            const encryptPassword = await UserController.encryptPassword(password);
+            const user_db = await UserModel.create({ username, email, password: encryptPassword });
             return res.status(200).json(user_db);
         } catch (error) {
             return res.status(500).send(error.message);
@@ -49,6 +51,48 @@ const UserController = {
             return res.status(500).send(error.message);
         }
     },
+    signIn: async (req, res) => {
+        const { email, password } = req.body;
+        
+        try {
+            const user_db = await UserModel.findOne({ email });
+
+            if(user_db === null){
+                return res.status(200).json("E-mail não cadastrado!");
+            }else{
+                const decryptPassword = await UserController.decryptPassword(user_db.password);
+                if(decryptPassword === password)
+                    return res.status(200).json("Usuário logado!");
+                else
+                    return res.status(200).json("Senha inválida!");                
+            }
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
+    },
+    encryptPassword: async (password) => {
+        const TYPE = process.env.ENCRYPT_TYPE;
+        const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPT_SECRET, 'base64');
+        const IV_LENGTH = 16;
+
+        let iv = crypto.randomBytes(IV_LENGTH);
+        let cipher = crypto.createCipheriv(process.env.ENCRYPT_ALGORITHM, Buffer.from(ENCRYPTION_KEY, TYPE), iv);
+        let encrypted = cipher.update(password);
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
+        return iv.toString(TYPE) + ':' + encrypted.toString(TYPE);
+    },
+    decryptPassword: async (password) => {
+        const TYPE = process.env.ENCRYPT_TYPE;
+        const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPT_SECRET, 'base64');
+
+        let textParts = password.split(':');
+        let iv = Buffer.from(textParts.shift(), TYPE);
+        let encryptedText = Buffer.from(textParts.join(':'), TYPE);
+        let decipher = crypto.createDecipheriv(process.env.ENCRYPT_ALGORITHM, Buffer.from(ENCRYPTION_KEY, TYPE), iv);
+        let decrypted = decipher.update(encryptedText);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted.toString();
+    }
 }
 
 module.exports = UserController;

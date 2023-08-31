0a520576a4e4f9d825e97dc8e73f432083dac51f
John Kennedy <johnkssilva@gmail.com>
2023-08-30 21:02:43 -0300

    Feat: add RecipeRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const UserRoutes = require('./user/UserRoutes');
 const ProductRoutes = require('./product/ProductRoutes');
+const RecipeRoutes = require('./recipe/RecipeRoutes');
 
 router.use(UserRoutes);
 router.use(ProductRoutes);
+router.use(RecipeRoutes);
 
 module.exports = router;

e64be08e0bac98b1b88fa40977e37b22ac1ae857
John Kennedy <johnkssilva@gmail.com>
2023-09-02 17:33:11 -0300

    Fix: free pass Access-Control-Allow-Origin to requisitions

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const dotenv = require("dotenv");
 const express = require('express');
 
 const app = express();
+
+// middlewares
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    next();
+});
+
 dotenv.config();
 
 const connectToDatabase = require("./src/database/connect");

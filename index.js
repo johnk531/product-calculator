eb1a017b2bc3e9ae5d2910eaf866a68e2b2dcf8a
John Kennedy <johnkssilva@gmail.com>
2023-09-05 21:46:45 -0300

    Chore: add header authorization in requisitions to API

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, Authorization, X-Requested-With, Content-Type, Accept');
+
+    console.log(req.headers.authorization);
 
     next();
 });

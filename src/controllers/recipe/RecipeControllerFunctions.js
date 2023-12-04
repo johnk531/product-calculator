91f0f6ade783c5b71b4a8922786e0f7c1bc708ae
John Kennedy <johnkssilva@gmail.com>
2023-12-03 22:20:38 -0300

    Feat: add initial recipe controller functions aux

---
+++ src/controllers/recipe/RecipeControllerFunctions.js
@@ -0,0 +1,9 @@
+const ProductModel = require("../../models/product/ProductModel");
+
+const RecipeControllerFunctions = {
+    ReturnCostRecipe: async (products) => {
+        return products;
+    }
+}
+
+module.exports = RecipeControllerFunctions;

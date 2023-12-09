833b7793e5583a4442b36b538bf0d4e9ff0172e1
John Kennedy <johnkssilva@gmail.com>
2023-12-09 19:00:53 -0300

    Feat: add returnValorQtd and return the products with name and valorQtd

--- src/controllers/recipe/RecipeControllerFunctions.js
+++ src/controllers/recipe/RecipeControllerFunctions.js
@@ -1,8 +1,22 @@
 const ProductModel = require("../../models/product/ProductModel");
 
 const RecipeControllerFunctions = {
-    ReturnCostRecipe: async (products) => {
-        return products;
+    ReturnCostRecipe: async (req, res) => {
+        try {
+            const products = req.body.products;
+            for (let i = 0; i < products.length; i++) {
+                const product = await ProductModel.findOne({ _id : products[i]._id });
+                products[i].nome = product.nome;
+                products[i].valorQtd = RecipeControllerFunctions.ReturnValorQtd(parseInt(product[i].quantidade), parseFloat(product[i].valor));
+            }
+            return res.status(200).json(products);
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
+    },
+    ReturnValorQtd: async (qtd, valor, qtdUsed) => {
+        const valorQtd = (valor * qtdUsed) / qtd; 
+        return valorQtd;
     }
 }
 

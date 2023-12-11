21569febab798ed6d0ed3a6f455f380af50e1ac2
John Kennedy <johnkssilva@gmail.com>
2023-12-11 18:31:42 -0300

    Feat: add test of returnValorQtd

--- __tests__/unit/recipe.test.js
+++ __tests__/unit/recipe.test.js
@@ -1,3 +1,5 @@
+const RecipeControllerFunctions = require('../../src/controllers/recipe/RecipeControllerFunctions');
+
 describe('Recipe', () => {
     it('should sum some values', () => {
         const x = 2;
@@ -6,5 +8,9 @@ describe('Recipe', () => {
         const sum = x + y;
 
         expect(sum).toBe(6);
+    });
+    it('should return the result of the rule of three', async () => {
+        const valorQtd = await RecipeControllerFunctions.ReturnValorQtd(1000, 25.50, 200);
+        expect(valorQtd).toBe((25.50 * 200) / 1000);
     });
 });
